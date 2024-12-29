75167275ab184bb7eb35a86cf856da41d333f505
michael <michael@localhost>
2024-12-29 13:06:45 +0100

    Lässt Stack Compose Datei anzeigen

--- src/app.py
+++ src/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, url_for
+from flask import Flask, render_template, redirect, url_for, request
 import json
 import dockercommands
 
@@ -11,11 +11,25 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return redirect(url_for("stacks"))
+    return redirect(url_for("swarmstacks"))
 
-@app.route("/stacks")
-def stacks():
-    return render_template("stacks.html", composeStacks=dockercommands.getComposeStacks(settings), swarmStacks=dockercommands.getSwarmStacks(settings))
+@app.route("/composestacks")
+def composestacks():
+    return render_template("composestacks.html", composeStacks=dockercommands.getComposeStacks(settings), errors=dockercommands.getLastStdErr())
+
+@app.route("/composestack")
+def composestack():
+    stackname = request.args.get("stackname")
+    return render_template("composestack.html", stackname=stackname, stack=dockercommands.getComposeStack(settings, stackname), errors=dockercommands.getLastStdErr())
+
+@app.route("/swarmstacks")
+def swarmstacks():
+    return render_template("swarmstacks.html", swarmStacks=dockercommands.getSwarmStacks(settings), errors=dockercommands.getLastStdErr())
+
+@app.route("/swarmstack")
+def swarmstack():
+    stackname = request.args.get("stackname")
+    return render_template("swarmstack.html", stackname=stackname, stack=dockercommands.getSwarmStack(settings, stackname), errors=dockercommands.getLastStdErr())
 
 if __name__ == "__main__":
     app.run(debug=True)
--- src/dockercommands.py
+++ src/dockercommands.py
@@ -1,14 +1,45 @@
-from subprocess import Popen, PIPE
+import subprocess
+from jinja2 import Template
+
+last_stderr = []
 
 def run_stdout(commandString):
-	process = Popen(["bash", "-c", commandString], stdout=PIPE)
-	result = ""
-	for line in process.stdout.readlines():
-		result = result + str(line)
-	return result
+	result = subprocess.run([commandString], shell=True, capture_output=True, text=True)
+	last_stderr.append(result.stderr.strip())
+	return result.stdout.strip()
+
+def getLastStdErr():
+	global last_stderr
+	last=last_stderr
+	last_stderr = []
+	return last
+
+def yaml_str_list(key, strList, list_as_yaml_object):
+	retStr = ""
+	if len(strList) > 0:
+		retStr = key + ":"
+		for item in strList:
+			retStr = retStr + "\n  - \"" + item + "\""
+	return retStr
+
+def obj_has_any_child(obj):
+	return len(dir(obj)) > 0
+
+def render_command(settings, commandTemplate, stackName):
+	command = Template(commandTemplate).render(stackname=stackName)
+	return command
+
+def asList(stdout):
+	return stdout.split("\n")
 
 def getSwarmStacks(settings):
-	return run_stdout(settings["commands"]["list_swarm_stacks"])
+	return asList(run_stdout(render_command(settings, settings["commands"]["list_swarm_stacks"], None)))
+
+def getSwarmStack(settings, stackName):
+	return run_stdout(render_command(settings, settings["commands"]["generate_compose_swarm_stack"], stackName))
 
 def getComposeStacks(settings):
-	return run_stdout(settings["commands"]["list_compose_stacks"])
+	return asList(run_stdout(render_command(settings, settings["commands"]["list_compose_stacks"], None)))
+
+def getComposeStack(settings, stackName):
+	return run_stdout(render_command(settings, settings["commands"]["generate_compose_stack"], stackName))
